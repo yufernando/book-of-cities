70cf399eaef20337dafd27c38a7c12e4c94df953
yufernando <yufernando@gmail.com>
2023-08-30 18:56:20 -0400

    feat(cities): add Asia, MENA and Oceania

--- config.py
+++ config.py
@@ -3,14 +3,17 @@ from layers.helpers import parse_user_input, read_cities_from_file
 
 # CITY_FILE = "data/cities_us.txt"
 # CITY_FILE = "data/cities_europe.txt"
-CITY_FILE = "data/cities_southamerica.txt"
+# CITY_FILE = "data/cities_southamerica.txt"
+CITY_FILE = "data/cities_asia.txt"
+# CITY_FILE = "data/cities_mena.txt"
+# CITY_FILE = "data/cities_oceania.txt"
 STREETS = True
 BUILDINGS = True
 MORPHOMETRICS = True
 FULL_VARIABLES = True
 CSV_OUT = False  # concatenate all morphometrics files into one CSV
-LOG_LEVEL = "INFO"
-# LOG_LEVEL = "DEBUG"
+# LOG_LEVEL = "INFO"
+LOG_LEVEL = "DEBUG"
 
 if parse_user_input() is not None:
     CITY_LIST = parse_user_input()
